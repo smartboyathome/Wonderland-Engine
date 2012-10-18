5d54d8cec4050e5a3fd404b11dbc6ab99abad60e
Smartboy <smartboyathome@gmail.com>
2012-10-18 15:22:46 -0700

    Forgot to put license disclaimers in some of the files.

--- tests/RestTests/test_rest_team_scores.py
+++ tests/RestTests/test_rest_team_scores.py
@@ -1,3 +1,23 @@
+'''
+    Copyright (c) 2012 Alexander Abbott
+
+    This file is part of the Cheshire Cyber Defense Scoring Engine (henceforth
+    referred to as Cheshire).
+
+    Cheshire is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Affero General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    Cheshire is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+    more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Cheshire.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import json
 from ScoringServer.utils import convert_datetime_to_timestamp
 from tests import show_difference_between_dicts
